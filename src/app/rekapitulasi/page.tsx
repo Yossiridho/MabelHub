c544416df1a4c1295dedd51fd854c8cb95bb1ef6
SakinahAulia-122140210 <sakinah.122140210@student.itera.ac.id>
2026-02-13 10:18:32 +0700

    commit

--- src/app/rekapitulasi/page.tsx
+++ src/app/rekapitulasi/page.tsx
@@ -223,7 +223,7 @@ export default function RekapitulasiVisitPage() {
       <div className="flex min-h-screen">
         <Sidebar role={role} />
 
-        <div className="flex-1 bg-[#F2F7FF] p-8">
+        <div className="flex-1 h-screen overflow-y-auto p-6">
           {/* HEADER */}
           <div className="mb-6 flex items-center gap-4">
             <h1 className="text-xl font-extrabold tracking-wide text-gray-900">
